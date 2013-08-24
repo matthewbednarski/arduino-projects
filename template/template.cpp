82bc196e80d0d2b3e8721f0c3e5d8992db59fe9b
Matthew Bednarski <matthewbednarski@gmail.com>
2013-08-24 12:10:52 +0200

    first commit of a panstamp|arduino compatible template project

---
+++ template/template.cpp
@@ -0,0 +1,71 @@
+#include <Arduino.h> 
+#include <EEPROM.h>
+#include "product.h"
+#include "panstamp.h"
+#include "regtable.h"
+
+
+void setup();
+void loop();
+
+
+/**
+ * setup
+ *
+ * Arduino setup function
+ */
+void setup()
+{
+  // Init panStamp
+  panstamp.init();
+  Serial.begin(9600);
+
+
+  // Transmit product code
+  getRegister(REGI_PRODUCTCODE)->getData();
+}
+
+/**
+ * loop
+ *
+ * Arduino main loop
+ */
+void loop()
+{
+  Serial.print("Product Code: ");
+  Serial.println(REGI_PRODUCTCODE);
+  getRegister(REGI_PRODUCTCODE)->getData();
+  delay(400);
+}
+
+/**
+ * Declaration of common callback functions
+ */
+DECLARE_COMMON_CALLBACKS()
+
+/**
+ * Definition of common registers
+ */
+DEFINE_COMMON_REGISTERS()
+
+/*
+ * Definition of custom registers
+ */
+// Your custom registers come here
+
+/**
+ * Initialize table of registers
+ */
+DECLARE_REGISTERS_START()
+  // Put here pointers to your custom registers
+DECLARE_REGISTERS_END()
+
+/**
+ * Definition of common getter/setter callback functions
+ */
+DEFINE_COMMON_CALLBACKS()
+
+/**
+ * Definition of custom getter/setter callback functions
+ */
+
