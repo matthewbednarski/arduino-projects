12ad35d42db05971edad5ef12361b77b0a9e2d57
matthew.bednarski@ekr.it <matthew.bednarski@ekr.it>
2016-02-04 07:02:37 +0100

    updated incorrect voltage in pin map table

--- sketches/IRrecvDemo/IRrecvDemo.cpp
+++ sketches/IRrecvDemo/IRrecvDemo.cpp
@@ -10,9 +10,16 @@ void loop() {
   //delay(100);
   static unsigned long last_time;
   unsigned long time = millis();
-  if( (time - last_time) > 500){
+  if( (time - last_time) > 100){
     if (irrecv.decode(&results)) {
-      Serial.println(results.value, HEX);
+      Serial.print("# len: ");
+      Serial.print(results.rawlen, DEC);
+      Serial.print(" -> value: ");
+      Serial.println(*results.rawbuf, DEC);
+      Serial.print("Decoded: ");
+      Serial.print(results.value, DEC);
+      Serial.print(" -> Type: ");
+      Serial.println(results.decode_type, DEC);
       irrecv.resume(); // Receive the next value
       last_time = time;
     }
